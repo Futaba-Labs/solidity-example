e157830b4d8fd084e0fd5a28791d6b3032e0043b
adachi_440 <tomoki1999314@gmail.com>
2023-09-07 11:01:12 +0700

    fix: gateway address

---
+++ tasks/setGateway.ts
@@ -0,0 +1,35 @@
+import { task, types } from "hardhat/config";
+import DEPLOYMENT from "../constants/deployment.json"
+
+task("TASK_SET_GATEWAY", "Set gateway contract address")
+  .addParam<string>("gateway", "Gateway contract address", "", types.string)
+  .setAction(
+    async (taskArgs, hre): Promise<null> => {
+      const deployment = DEPLOYMENT[hre.network.name as keyof typeof DEPLOYMENT]
+      const gateway = taskArgs.gateway
+
+      const balanceQuery = await hre.ethers.getContractAt("BalanceQuery", deployment.balance)
+      const customQuery = await hre.ethers.getContractAt("CustomQuery", deployment.custom)
+      const voting = await hre.ethers.getContractAt("Voting", deployment.voting)
+      try {
+        console.log(`Set gateway on BalanceQuery...`)
+        let tx = await (await balanceQuery.setGateway(gateway, { gasLimit: 2000000 })).wait()
+        console.log(`tx: ${tx.transactionHash}`)
+        console.log("Set gateway on CustomQuery...")
+        tx = await (await customQuery.setGateway(gateway, { gasLimit: 2000000 })).wait()
+        console.log(`tx: ${tx.transactionHash}`)
+        console.log("Set gateway on Voting...")
+        tx = await (await voting.setGateway(gateway, { gasLimit: 2000000 })).wait()
+        console.log(`tx: ${tx.transactionHash}`)
+        console.log(`✅ [${hre.network.name}] setGateway(${gateway})`)
+      } catch (e: any) {
+        if (e.error.message.includes("The chainId + address is already trusted")) {
+          console.log("*source already set*")
+        } else {
+          console.log(e)
+          console.log(`❌ [${hre.network.name}] setGateway(${gateway})`)
+        }
+      }
+      return null;
+    }
+  );
