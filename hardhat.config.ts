e33acf0d65eb7a4d8611a67135bcda7708a4d4d6
adachi_440 <tomoki1999314@gmail.com>
2023-09-22 02:46:50 +0800

    remove foundry

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,6 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import * as dotenv from 'dotenv';
 import "./tasks/index";
-import "@nomicfoundation/hardhat-foundry";
 
 dotenv.config();
 const accounts =
